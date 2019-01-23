d433283d03f69124e07c80776bbabff8bab4536a
gatco_chiraz <thecode371@hotmail.com>
2019-01-23 17:06:06 +0200

    First commit

---
+++ reflexit/ui/authentication/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.reflexit.reflexit.ui.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
